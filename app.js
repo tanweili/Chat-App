c209a6cedaf5b89fbccbb6d72220f54a320bea58
Wei Li Tan <>
2022-08-31 15:16:06 +0800

    Add back params to URL so that .js script can get variables

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express')
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-const { Server } = require("socket.io")
+const { Server } = require("socket.io");
+const { SocketAddress } = require('net');
 const io = new Server(server);
 
 app.use(express.urlencoded({extended: false}))
@@ -16,23 +17,25 @@ app.get('/', (req, res) => {
     res.render('login.ejs')
 })
 
-app.post('/room', (req, res) => {
-    res.render('room.ejs', {
-        roomname: req.body.roomname,
-        username: req.body.username
-    })
+app.get('/room', (req, res) => {
+    res.render('room')
 })
 
-// app.post('/room', (req, res) => {
-
-//     res.redirect(`/room?username=${username}&roomname=${roomname}`)
-// })
+app.post('/room', (req, res) => {
+    username = req.body.username,
+    roomname = req.body.roomname
+    res.redirect(`/room?username=${username}&roomname=${roomname}`)
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
     socket.on('disconnect', () => {
         console.log('user DCed..')
+    })
+
+    socket.on('userJoined', ({username, roomname}) => {
+        console.log(`${username} joined ${roomname}`)
     })
 });
 
--- public/room.js
+++ public/room.js
@@ -1,1 +1,7 @@
-var socket = io()
+const URLparams = new URLSearchParams(window.location.search)
+const username = URLparams.get('username')
+const roomname = URLparams.get('roomname')
+
+const socket = io()
+
+socket.emit('userJoined',({username, roomname}))
