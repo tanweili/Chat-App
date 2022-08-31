9225b91dfa15b1e7708ef9078451c32c02c326d9
Wei Li Tan <>
2022-08-31 16:33:36 +0800

    Able to update webpage with new messages, however not shared via server yet

--- public/room.js
+++ public/room.js
@@ -5,3 +5,12 @@ const roomname = URLparams.get('roomname')
 const socket = io()
 
 socket.emit('userJoined',({username, roomname}))
+
+const sendButton = document.getElementById('send')
+sendButton.addEventListener('click', (e) => {
+    const chatLog = document.getElementById('chatLog')
+    const message = document.getElementById('message').value
+    var newMessage = document.createElement("div")
+    newMessage.innerText = message;
+    chatLog.appendChild(newMessage)
+})
