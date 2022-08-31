d9921838b4330d9985aa037ea515221efa0e9644
Wei Li Tan <>
2022-08-31 14:30:15 +0800

    user information passed to POST route via req.body

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Chat-App
-To implement a chat application using front end technologies
+# Chat-App  
+To implement a chat application using front end technologies  
 
-Tutorial websites referenced:
-https://socket.io/get-started/chat
-https://betterprogramming.pub/building-a-chat-application-from-scratch-with-room-functionality-df3d1e4ef662
+Tutorial websites referenced:  
+https://socket.io/get-started/chat  
+https://betterprogramming.pub/building-a-chat-application-from-scratch-with-room-functionality-df3d1e4ef662  
+https://www.cometchat.com/tutorials/how-to-build-a-chat-app-with-websockets-and-node-js  
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require ('path')
 const express = require('express')
 const app = express();
 const http = require('http');
@@ -6,7 +7,7 @@ const { Server } = require("socket.io")
 const io = new Server(server);
 
 app.use(express.urlencoded({extended: false}))
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs')
 
 port = 8888
@@ -15,18 +16,21 @@ app.get('/', (req, res) => {
     res.render('login.ejs')
 })
 
-app.get('/room', (req, res) => {
-    res.render('room.ejs')
+app.post('/room', (req, res) => {
+    res.render('room.ejs', {
+        roomname: req.body.roomname,
+        username: req.body.username
+    })
 })
 
-app.post('/room', (req, res) => {
-    roomname = req.body.roomname;
-    username = req.body.username;
-    res.redirect(`/room?username=${username}&roomname=${roomname}`)
-})
+// app.post('/room', (req, res) => {
+
+//     res.redirect(`/room?username=${username}&roomname=${roomname}`)
+// })
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
     socket.on('disconnect', () => {
         console.log('user DCed..')
     })
